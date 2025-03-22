8b8c6fd71ce95b7a308bde6bb78b80c98b150cd0
E M <46611368+lKinderBueno@users.noreply.github.com>
2025-03-22 20:24:11 +0100

    Update proxy.js

--- routes/proxy.js
+++ routes/proxy.js
@@ -1,20 +1,26 @@
-const { default: axios } = require("axios");
-
 module.exports = async (router, options) => {
     router.post('/proxy', async (req, res) => {
         const url = req.body.url;
-        console.log(`DOWNLOADING: ${url}\n`)
-        const data = await axios.get(url.trim(), {
+        console.log(`DOWNLOADING: ${url}\n`);
+
+        try {
+            const response = await fetch(url.trim(), {
                 headers: {
                     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"
                 },
-                timeout: 360000, // 6 minutes
-            }).then(result => result.data)
-            .catch(err => {
-                console.log(`${err && err.message ? err.message : "Unknown error: "}: ${url}\n`)
+                timeout: 360000 // 6 minutes
             });
-        if (data)
-            res.send(data)
-        else res.status(404).send()
+
+            if (!response.ok) {
+                console.log(`Error: ${response.statusText}: ${url}\n`);
+                return res.status(response.status).send(response.statusText);
+            }
+
+            const data = await response.text();
+            res.send(data);
+        } catch (err) {
+            console.log(`${err && err.message ? err.message : "Unknown error"}: ${url}\n`);
+            res.status(404).send();
+        }
     });
 }
