19ee9ab3ab45f549d4be1847ffaeb0d59ed8eff4
lKinderBueno <46611368+lKinderBueno@users.noreply.github.com>
2025-09-16 00:38:09 +0200

    refactor: streamline user agent generation and improve logging in proxy route

--- routes/proxy.js
+++ routes/proxy.js
@@ -1,8 +1,36 @@
+const generateRandomUserAgent = () => {
+    const userAgents = ["okhttp/_libv_", "TiviMate/_libv_ (_device_; Android _andv_)"]
+    const androidTvDevices = [
+        "Amazon AFTMM",
+        "NVIDIA Shield TV",
+        "Google Chromecast with Google TV",
+        "Xiaomi Mi Box S",
+        "Sony Bravia Android TV",
+        "Philips Android TV",
+        "TCL Android TV",
+        "Hisense Android TV",
+        "Sharp Android TV",
+        "OnePlus TV"
+    ];
+
+    const androidVersion = `${getRandomNumberInRange(6, 14)}.${getRandomNumberInRange(0, 2)}.${getRandomNumberInRange(0, 2)}`;
+    const libVersion = `${getRandomNumberInRange(2, 4)}.${getRandomNumberInRange(0, 20)}.${getRandomNumberInRange(0, 20)}`;
+
+    const randomUAIndex = Math.floor(Math.random() * userAgents.length);
+    const randomDeviceIndex = Math.floor(Math.random() * androidTvDevices.length);
+    return userAgents[randomUAIndex].replace("_libv_", libVersion).replace("_andv_", androidVersion).replace("_device_", androidTvDevices[randomDeviceIndex]);
+}
+
+function getRandomNumberInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 module.exports = async (router, options) => {
+    const useragent = generateRandomUserAgent();
+    console.log(`Generated User-Agent: ${useragent}\n`);
     router.post('/proxy', async (req, res) => {
-        const useragent = generateRandomUserAgent();
         const url = req.body.url;
-        console.log(`DOWNLOADING: ${url} with User-Agent: ${useragent}\n`);
+        console.log(`DOWNLOADING: ${url}\n`);
 
         try {
             const response = await fetch(url.trim(), {
@@ -24,31 +52,4 @@ module.exports = async (router, options) => {
             res.status(404).send();
         }
     });
-}
-
-const generateRandomUserAgent = () => {
-    const userAgents = ["okhttp/_libv_","TiviMate/_libv_ (_device_; Android _andv_)"]
-    const androidTvDevices = [
-        "Amazon AFTMM",
-        "NVIDIA Shield TV",
-        "Google Chromecast with Google TV",
-        "Xiaomi Mi Box S",
-        "Sony Bravia Android TV",
-        "Philips Android TV",
-        "TCL Android TV",
-        "Hisense Android TV",
-        "Sharp Android TV",
-        "OnePlus TV"
-    ];
-
-    const androidVersion = `${getRandomNumberInRange(6,14)}.${getRandomNumberInRange(0,2)}.${getRandomNumberInRange(0,2)}`;
-    const libVersion = `${getRandomNumberInRange(2,4)}.${getRandomNumberInRange(0,20)}.${getRandomNumberInRange(0,20)}`;
-
-    const randomUAIndex = Math.floor(Math.random() * userAgents.length);
-    const randomDeviceIndex = Math.floor(Math.random() * androidTvDevices.length);
-    return userAgents[randomUAIndex].replace("_libv_", libVersion).replace("_andv_", androidVersion).replace("_device_", androidTvDevices[randomDeviceIndex]);
-}
-
-function getRandomNumberInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
